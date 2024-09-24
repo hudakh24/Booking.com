0d2b6f81b7597836f34938df29840decf338cfcb
hudakh24 <hudashoaib2000@gmail.com>
2024-09-24 17:01:55 +0500

    24:09:24-rooms availabel API Integrated

--- front-end/src/pages/Rooms/Rooms.jsx
+++ front-end/src/pages/Rooms/Rooms.jsx
@@ -4,11 +4,14 @@ import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/Header/Header"; 
 import SideBarSearch from "../../components/SideBarSearch/SideBarSearch" // Import SidebarSearch
 import Footer from "../../components/Footer/Footer"; 
+import { useLocation } from "react-router-dom";
 
 const Rooms = () => {
+  const location = useLocation();
+  console.log("roomsLoc-->",location.state)
   return (
     <>
-      <div className="home">
+      <div className="home"> 
         <Navbar />
         <Header />
         <SideBarSearch /> {/* Add the SidebarSearch here */}
